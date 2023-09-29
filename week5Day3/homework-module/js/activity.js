d4f6a68c5ba41198affd2cceb55deb03f7650a3e
Iffy Eleweke <omaleweks@gmail.com>
2023-09-29 17:31:45 -0400

    Added the names of the cliffs to the activities selected

--- week5Day3/homework-module/js/activity.js
+++ week5Day3/homework-module/js/activity.js
@@ -1,11 +1,15 @@
 /* Using jQuery to highlight cells in the table */
 $(document).ready(function() {
+    // Define cliff site names
+    var cliffSites = ["West Cliff", "North Cliff", "East Cliff", "South Cliff"];
+
     // Remove initial selected class
     $("table tbody td.selected").removeClass("selected");
 
     // Function handle cell click
     $("table tbody td").click(function() {   // user select a table data cell
         var content = $(this).text();    // get content of cell
+        var columnIndex = $(this).index(); // Get the column index
 
         // check if the cell is selectable 
         if (!$(this).hasClass("not-selectable")) {
@@ -13,18 +17,25 @@ $(document).ready(function() {
             $(this).toggleClass("selected");
 
             if($(this).hasClass("selected")) {     // check if selected cell has class
-                $('#displaySelected').css("visibility","visible");   // make display box visible 
-                $('#displaySelected').css("margin-top","2em");    // add spaces above display box 
-                $('#result').append("<p>" + content + "</p>");   // add child element with contents of cell 
-            } else {   // if selected cell don't have class 
-                $('#result p:contains(' + content + ')').remove();  // remove child element 
+                // Get the cliff site name based on the column index
+                var cliffSite = cliffSites[columnIndex - 1]; // Subtract 1 for activity column
 
-                if($('#result').has('p').length == false) {   // check if there are any child elements within parent 
-                    $('#displaySelected').css("visibility","hidden");   // make display box hidden 
-                    $('#displaySelected').css("margin-top","0");    // remove spaces above display box 
+                $('#displaySelected').css("visibility", "visible");   // make display box visible 
+                $('#displaySelected').css("margin-top", "2em");    // add spaces above display box 
+
+                // Append selected activity and cliff site to result
+                $('#result').append("<p>" + content + " at " + cliffSite + "</p>");    // add child element with contents of cell 
+            } else {   
+                // if selected cell don't have class 
+                // Remove the selected activity from result
+                $('#result p:contains(' + content + ')').remove();
+
+                if($('#result').has('p').length === 0) {   // check if there are any child elements within parent 
+                    // Hide the display box if no selected activities
+                    $('#displaySelected').css("visibility", "hidden");   // make display box hidden
+                    $('#displaySelected').css("margin-top", "0");    // remove spaces above display box 
                 }
             } 
         }
     });
 });
-
