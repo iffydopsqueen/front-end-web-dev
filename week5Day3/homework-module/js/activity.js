0e66eb1856bd5dad89a646fea8d0d4a19c6c2c4d
Iffy Eleweke <omaleweks@gmail.com>
2023-09-29 16:57:38 -0400

    Display selcted table cells as a list below the activities table

--- week5Day3/homework-module/js/activity.js
+++ week5Day3/homework-module/js/activity.js
@@ -4,11 +4,26 @@ $(document).ready(function() {
     $("table tbody td.selected").removeClass("selected");
 
     // Function handle cell click
-    $("table tbody td").click(function() {
+    $("table tbody td").click(function() {   // user select a table data cell
+        var content = $(this).text();    // get content of cell
+
         // check if the cell is selectable 
         if (!$(this).hasClass("not-selectable")) {
             // toggle cell selection
             $(this).toggleClass("selected");
+
+            if($(this).hasClass("selected")) {     // check if selected cell has class
+                $('#displaySelected').css("visibility","visible");   // make display box visible 
+                $('#displaySelected').css("margin-top","2em");    // add spaces above display box 
+                $('#result').append("<p>" + content + "</p>");   // add child element with contents of cell 
+            } else {   // if selected cell don't have class 
+                $('#result p:contains(' + content + ')').remove();  // remove child element 
+
+                if($('#result').has('p').length == false) {   // check if there are any child elements within parent 
+                    $('#displaySelected').css("visibility","hidden");   // make display box hidden 
+                    $('#displaySelected').css("margin-top","0");    // remove spaces above display box 
+                }
+            } 
         }
     });
 });
